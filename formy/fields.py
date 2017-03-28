f1fd7fa3f8ed83cfd8076eff8e8f1e6b5a252cae
Brian Jinwright <bjinwright@qwigo.com>
2017-03-27 23:51:17 -0400

    integrated fully with valley

--- formy/fields.py
+++ formy/fields.py
@@ -1,18 +1,10 @@
-import collections
-
-import datetime
-
-import six
-from valley import BooleanValidator
-from valley import ValidationException
-from valley.mixins import VariableMixin, \
-    CharVariableMixin, IntegerVariableMixin, \
-    FloatVariableMixin, BooleanMixin, \
-    DateMixin, DateTimeMixin, SlugVariableMixin, EmailVariableMixin
+from valley.mixins import SlugVariableMixin, EmailVariableMixin
+from valley.properties import *
 
 from formy.templates.fields import base_field_template
 
-class BaseField(VariableMixin, object):
+
+class BaseFieldMixin(object):
     template = base_field_template
     css_classes = ''
     value = None
@@ -28,6 +20,7 @@ class BaseField(VariableMixin, object):
         css_classes=None,
         **kwargs
     ):
+
         self.default_value = default_value
         self.required = required
         self.kwargs = kwargs
@@ -38,22 +31,6 @@ class BaseField(VariableMixin, object):
         self.validators = set(self.validators)
         if verbose_name:
             self.verbose_name = verbose_name
-
-    def validate(self, value, key):
-        if not value and not isinstance(self.get_default_value(), type(None)):
-            value = self.get_default_value()
-        for i in self.validators:
-            i.validate(value, key)
-
-    def get_default_value(self):
-        """ return default value """
-        default = self.default_value
-        if isinstance(default, collections.Callable):
-            default = default()
-        return default
-
-    def get_python_value(self, value):
-        return value
 
     def render(self,name=None,value=None,css_classes=None,input_type=None):
         if not name:
@@ -73,137 +50,31 @@ class BaseField(VariableMixin, object):
         )
 
 
-class CharField(CharVariableMixin, BaseField):
+class CharField(BaseFieldMixin,CharProperty):
+    pass
 
-    def get_python_value(self, value):
-        if not value:
-            return None
-        return str(value)
-
-
-class SlugField(SlugVariableMixin,CharField):
+class SlugField(BaseFieldMixin,SlugVariableMixin,CharProperty):
     pass
 
 
-class EmailField(EmailVariableMixin,CharField):
+class EmailField(BaseFieldMixin,EmailVariableMixin,CharProperty):
     input_type = 'email'
 
 
-class IntegerField(IntegerVariableMixin, BaseField):
+class IntegerField(BaseFieldMixin,IntegerProperty):
     input_type = 'number'
 
-    def get_python_value(self, value):
-        if not value:
-            return None
-        return int(value)
 
-
-class FloatField(FloatVariableMixin, BaseField):
+class FloatField(BaseFieldMixin, FloatProperty):
     input_type = 'number'
 
-    def get_python_value(self, value):
-        if not value:
-            return None
-        return float(value)
 
-
-class BooleanField(BooleanMixin, BaseField):
+class BooleanField(BaseFieldMixin,BooleanProperty):
     input_type = 'checkbox'
 
-    def get_python_value(self, value):
-        if not value:
-            return False
-        try:
-            BooleanValidator().validate(value, 'boolean')
-        except ValidationException:
-            return value
-        return bool(value)
 
-
-class DateField(DateMixin, BaseField):
+class DateField(BaseFieldMixin, DateProperty):
     input_type = 'date'
-    def __init__(
-            self,
-            default_value=None,
-            required=True,
-            validators=[],
-            verbose_name=None,
-            auto_now=False,
-            auto_now_add=False,
-            **kwargs):
 
-        super(
-            DateField,
-            self).__init__(
-            default_value=default_value,
-            required=required,
-            validators=validators,
-            verbose_name=verbose_name,
-            **kwargs)
-        self.auto_now = auto_now
-        self.auto_now_add = auto_now_add
-
-    def now(self):
-        return datetime.datetime.now().date()
-
-    def get_default_value(self):
-        default = self.default_value
-        if self.auto_now or self.auto_now_add:
-            return self.now()
-        return default
-
-    def get_python_value(self, value):
-        if not value:
-            return None
-        if isinstance(value, six.string_types):
-            try:
-                value = datetime.date(*datetime.time.strptime(value, '%Y-%m-%d')[:3])
-            except ValueError as e:
-                raise ValueError('Invalid ISO date %r [%s]' % (value,
-                                                               str(e)))
-        return value
-
-
-class DateTimeField(DateTimeMixin, BaseField):
+class DateTimeField(BaseFieldMixin,DateTimeProperty):
     input_type = 'datetime-local'
-    def __init__(
-            self,
-            default_value=None,
-            required=True,
-            validators=[],
-            verbose_name=None,
-            auto_now=False,
-            auto_now_add=False,
-            **kwargs):
-
-        super(
-            DateTimeField,
-            self).__init__(
-            default_value=default_value,
-            required=required,
-            validators=validators,
-            verbose_name=verbose_name,
-            **kwargs)
-        self.auto_now = auto_now
-        self.auto_now_add = auto_now_add
-
-    def get_default_value(self):
-        default = self.default_value
-        if self.auto_now or self.auto_now_add:
-            return self.now()
-        return default
-
-    def get_python_value(self, value):
-        if isinstance(value, six.string_types):
-            try:
-                value = value.split('.', 1)[0]  # strip out microseconds
-                value = value[0:19]  # remove timezone
-                value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
-            except ValueError as e:
-                raise ValueError('Invalid ISO date/time %r [%s]' %
-                                 (value, str(e)))
-        return value
-
-    def now(self):
-        return datetime.datetime.utcnow()
-
--- formy/form.py
+++ formy/form.py
@@ -1,65 +1,34 @@
 from six import with_metaclass
 from valley.declarative import DeclaredVars as DV, \
     DeclarativeVariablesMetaclass as DVM
+from valley.schema import BaseSchema
 from valley.utils import import_util
-from .fields import BaseField
-
-
-BUILTIN_DOC_ATTRS = ('_id', '_doc_type')
+from .fields import BaseFieldMixin
 
 
 class DeclaredVars(DV):
-    base_field_class = BaseField
-    base_field_type = '_base_fields'
+    base_field_class = BaseFieldMixin
 
 
 class DeclarativeVariablesMetaclass(DVM):
     declared_vars_class = DeclaredVars
 
 
-class BaseForm(object):
+class BaseForm(BaseSchema):
     """
     Base class for all Formy form classes.
     """
     template = 'formy.templates.base_template'
 
-    def __init__(self, **kwargs):
-        self.form_data = self.process_form_kwargs(kwargs)
-
-    def __repr__(self):
-        return '<{class_name}: {uni} >'.format(
-            class_name=self.__class__.__name__, uni=self.__unicode__())
-
-    def __unicode__(self):
-        return '({0})'.format(self.__class__.__name__)
-
     def __iter__(self):
-        for k, field in self._base_fields.items():
+        for k, field in self._base_properties.items():
             field.name = k
-            field.value = self.form_data.get(k)
+            field.value = self._data.get(k)
             yield field
-
-    def process_form_kwargs(self, kwargs):
-        doc = {}
-        for key, prop in list(self._base_fields.items()):
-            try:
-                value = prop.get_python_value(kwargs.get(key) or prop.get_default_value())
-            except ValueError:
-                value = kwargs.get(key) or prop.get_default_value()
-
-            doc[key] = value
-        for i in BUILTIN_DOC_ATTRS:
-            if kwargs.get(i):
-                doc[i] = kwargs[i]
-        return doc
 
     def render(self,include_submit=True):
         template = import_util(self.template)
         return template.render(form=self,include_submit=include_submit)
-
-    def validate(self):
-        for k,v in self._base_fields.items():
-            v.validate(self.form_data.get(k),k)
 
 
 class Form(with_metaclass(DeclarativeVariablesMetaclass, BaseForm)):
