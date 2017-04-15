46cc22dab0833376ec44650befa48d1521083a1d
Brian Jinwright <bjinwright@qwigo.com>
2017-04-14 22:48:20 -0400

    Added integration with KEV, added KEVForm, added errors to field template, added render_static_assets added CKEditorField, and TextField

--- formy/__init__.py
+++ formy/__init__.py
@@ -1,4 +1,4 @@
 from .form import Form
 from .fields import (CharField, IntegerField, BooleanField,
                      DateField,ChoiceField,MultipleChoiceField,FloatField,DateTimeField,EmailField,
-                     SlugField)
+                     SlugField,TextAreaField,CKEditor)
--- formy/templates/fields.py
+++ formy/templates/fields.py
@@ -2,10 +2,8 @@ from jinja2 import Template
 
 base_field_template = Template(
     """
-    {% if errors %}
-    {% for error in errors %}
-    <p>{{error}}</p>
-    {% endfor %}
+    {% if error %}
+    <div class="alert alert-danger" role="alert">{{error}}</div>
     {% endif %}
     <input type="{{input_type}}" name="{{name}}" {% if value %}value="{{value}}"{% endif %} {% if css_classes %}class="{{css_classes}}"{% endif %} {% if placeholder %}placeholder={{placeholder}}{% endif %}>
     """
@@ -13,10 +11,8 @@ base_field_template = Template(
 
 select_field_template = Template(
     """
-    {% if errors %}
-    {% for error in errors %}
-    <p>{{error}}</p>
-    {% endfor %}
+    {% if error %}
+    <div class="alert alert-danger" role="alert">{{error}}</div>
     {% endif %}
     <select name="{{name}}" {% if css_classes %}class="{{css_classes}}"{% endif %}>
     {% for k,v in choices.items() %}
@@ -28,15 +24,38 @@ select_field_template = Template(
 
 select_multiple_field_template = Template(
     """
-    {% if errors %}
-    {% for error in errors %}
-    <p>{{error}}</p>
-    {% endfor %}
+    {% if error %}
+    <div class="alert alert-danger" role="alert">{{error}}</div>
     {% endif %}
     <select name="{{name}}" {% if css_classes %}class="{{css_classes}}"{% endif %} multiple>
     {% for k,v in choices.items() %}
         <option value="{{v}}" {% if value == v %}selected{% endif %}>{{k}}</option>
     {% endfor %}
     </select>
+    """
+)
+
+textarea_field_template = Template(
+    """
+    {% if error %}
+    <div class="alert alert-danger" role="alert">{{error}}</div>
+    {% endif %}
+    <textarea name="{{name}}" rows="10" cols="30" {% if css_classes %}class="{{css_classes}}"{% endif %} {% if placeholder %}placeholder={{placeholder}}{% endif %}>
+    {% if value %}{{value}}{% endif %}
+    </textarea>
+    """
+)
+
+ckeditor_field_template = Template(
+    """
+    {% if error %}
+    <div class="alert alert-danger" role="alert">{{error}}</div>
+    {% endif %}
+    <textarea name="{{name}}" rows="10" id="id_{{name}}" cols="30" {% if css_classes %}class="{{css_classes}}"{% endif %} {% if placeholder %}placeholder={{placeholder}}{% endif %}>
+    {% if value %}{{value}}{% endif %}
+    </textarea>
+    <script>
+                CKEDITOR.replace( 'id_{{name}}' );
+            </script>
     """
 )
